3713436b366d992fa59290772aac84e9f7308dcd
Kaja Kristiansen <kaja@potepels.com>
2019-09-05 15:21:06 +0200

    Yaaaz masse fine greier. Jobber med basge-siden

--- src/components/partnerRow/PartnerRow.js
+++ src/components/partnerRow/PartnerRow.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './PartnerRow.scss';
-// import Prototype from '../../data/prototype.json';
+import SiteData from '../../data/site.json';
 import Itera from '../../assets/images/logos/itera-logo.png';
 import Inmeta from '../../assets/images/logos/inmeta-logo.png';
 
 class PartnerRow extends Component {
 
+  constructor() {
+    super();
+    this.getIcon = this.getIcon.bind(this);
+}
+
+  getIcon(logo) {
+    switch (logo) {
+    case 'itera-partner':
+        return Itera;
+    case 'inmeta-partner':
+        return Inmeta;
+    default:
+        return null;
+    }
+}
 
   render() {
 
@@ -14,20 +28,18 @@ class PartnerRow extends Component {
       <header className="c_partner-row">       
         <div className="g_center">
           <div className="c_partner-row__content">
+          <h3 className="c_partner-row__heading">Our awesome partners:</h3>
             <ul className="c_partner-row__logos">
-              <li>
-                <Link to="http://itera.no" aria-label="Visit Itera.no">
-                  <img src={Itera} alt="Itera logo" />
-                  Itera
-                </Link>
+            {SiteData.partnerRow.map(item => 
+            (
+              <li key={item.id}>
+                <a href="http://itera.no" aria-label="Visit Itera.no" target="_blank" rel="noopener noreferrer">
+                  <img src={this.getIcon(item.id)} alt="Itera logo" />
+                </a>
               </li>
-              <li>
-                <Link to="http://inemta.no" aria-label="Visit Inmeta.no">
-                  <img src={Inmeta} alt="Inmeta logo" />
-                  Inmeta
-                </Link>
-              </li>
+            ))}
             </ul>
+            <h3 className="c_partner-row__heading">Thank you so much!</h3>
           </div>
         </div>
       </header>
--- src/pages/badges/Badges.js
+++ src/pages/badges/Badges.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from '../../components/nav/Nav';
 import Header from '../../components/header/Header';
+import BadgesData from '../../data/badges.json';
+import Badge from '../../components/badge/Badge';
 import './Badges.scss';
 
 export default class Badges extends React.Component {
@@ -14,6 +16,14 @@ export default class Badges extends React.Component {
 					theme="turquoise-darkblue" />
 				<div className="g_center">
 					<h2>Badges</h2>
+					<div className="badge-list">
+						{BadgesData.badges.map(item => 
+						(
+							<Badge
+								item={item}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 		);
