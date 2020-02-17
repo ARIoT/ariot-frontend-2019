dcbc2466b484a96ee7a7b35fe9c44e2c749a4c1d
potepels <kaja@potepels.com>
2020-02-17 16:07:10 +0100

    nå med leaderborardsdsadsad

--- src/pages/pastEvents/LastYear.js
+++ src/pages/pastEvents/LastYear.js
@@ -4,6 +4,7 @@ import Header from '../../components/layout/header/Header';
 import Footer from '../../components/layout/footer/Footer';
 import PartnerRow from '../../components/partnerRow/PartnerRow';
 import LabelTextBox from '../../components/labelTextBox/LabelTextBox';
+import Leaderboard from 'components/leaderboard/Leaderboard';
 import pastEventsData from '../../data/pastEvents.json';
 import './LastYear.scss';
 
@@ -25,17 +26,23 @@ export default class LastYear extends React.Component {
 							type="logo"
 							theme="pink-darkblue" />
 						<div className="g_center">
-							<h2 className="year-title">{thisYear}</h2>   
-							<div className="u_label-text-boxes u_label-text-boxes--three">
-								{thisYearData['topBoxes'] && (
-									thisYearData['topBoxes'].map((item, index) => (
+							<h2 className="year-title">...back to {thisYear}</h2>
+							{thisYearData['topBoxes'] && (
+								<div className="u_label-text-boxes u_label-text-boxes--three">
+									{thisYearData['topBoxes'].map((item, index) => (
 										<LabelTextBox
-												item = {item}
-												key = {`topBoxes-${item}`}
-										/>
-									)
-								))}
-							</div>
+											item = {item}
+											key = {`topBoxes-${index}`}
+									/>
+									))}
+								</div>	
+							)}
+
+							{thisYearData['leaderboard'] && (
+								<Leaderboard
+									item = {thisYearData['leaderboard']} />
+								
+							)}
 						</div>
 					</div>
 					<PartnerRow />
